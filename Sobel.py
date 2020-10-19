dffa994f76da4d683890682e8e0f8c5fb5f87a09
Crehul Vlad <34303919+VladC12@users.noreply.github.com>
2020-10-19 18:25:16 +0300

    Update Sobel.py

--- Sobel.py
+++ Sobel.py
@@ -4,7 +4,7 @@ import numpy as np
 
 ###Edge Detection Algotirhm (Sobel Operator)
 
-InputFile="E:/Facultate/2020-2021_Sem1_An_IV/PNI/AnacondaStuff/_TestImagesPNI_labs/ImgTstColor/avion_a.jpg" 
+InputFile="E:/Facultate/2020-2021_Sem1_An_IV/PNI/AnacondaStuff/_TestImagesPNI_labs/ImgTstColor/Lenna.bmp" 
 #file name here 
 #TODO: change the constant file location to a command line argument and then in the future argument for program
 InputRGB = cv2.imread(InputFile)
@@ -26,36 +26,85 @@ plt.show()
 ImageLengthX=InputGray.shape[0] #horizontal length of image
 ImageLengthY=InputGray.shape[1] #vertical length of image
 
+(thresh, InputBW) = cv2.threshold(InputGray, 127, 255, cv2.THRESH_BINARY) #converting to black and white
+
+plt.figure()
+plt.imshow(InputBW,cmap = 'gray')
+plt.show()
+
 ##Applying masks
 #Apply a mask in X
-maskx = np.array([[-1, -2, -1], #3x3 Sobel Operator 
-                  [ 0,  0,  0], #TODO: Upgrade to 5x5, 7x7, etc.
-                  [-1, -2, -1]])
+maskx = np.array([[-1, 0, 1],  #3x3 Sobel Operator
+                  [-2, 0, 2],  #TODO: Upgrade to 5x5, 7x7, etc.
+                  [-1, 0, 1]])
+
+SobelX = np.zeros((ImageLengthX,ImageLengthY), dtype=int)
 
 i=0 
 j=0
-InputGrayMini = np.array([[InputGray.item(i-1, j-1),InputGray.item(i-1, j),InputGray.item(i-1, j+1)],
-                          [InputGray.item(i,   j-1),InputGray.item(i,   j),InputGray.item(i,   j+1)],
-                          [InputGray.item(i+1, j-1),InputGray.item(i+1, j),InputGray.item(i+1, j+1)]])
+for i in range(ImageLengthX-1):
+    for j in range(ImageLengthY-1):
+        InputMini = np.array([[InputBW.item(i-1, j-1),InputBW.item(i-1, j),InputBW.item(i-1, j+1)],
+                              [InputBW.item(i,   j-1),InputBW.item(i,   j),InputBW.item(i,   j+1)],
+                              [InputBW.item(i+1, j-1),InputBW.item(i+1, j),InputBW.item(i+1, j+1)]])
+        #edge (literally) case
+        if i-1 < 0:
+            InputMini[0,:] = 0
+        if j-1 < 0:
+            InputMini[:,0] = 0
+        if i+1 > ImageLengthX:
+            InputMini[2,:] = 0
+        if j+1 >ImageLengthY:
+            InputMini[:,2] = 0
+        
+        SobVal = np.sum(np.multiply(InputMini,maskx))
+        if SobVal < 0:
+            SobelX[i,j] = 255
+        elif SobVal >= 0:
+            SobelX[i,j] = 0
 
-if i-1 < 0:
-    InputGrayMini[i-1,:] = 0
-#if j-1 < 0:
-#    InputGrayMini[j-1,:] = 0
-#if i+1 > ImageLengthX:
-#    InputGrayMini[i+1,:] = 0
-#if j+1 >ImageLengthY:
-#    InputGrayMini[j+1,:] = 0
-    
-test = np.multiply(InputGrayMini,maskx)
-x = np.sum(np.multiply(InputGrayMini,maskx))
+Gx = np.array(SobelX)
+plt.figure()
+plt.imshow(Gx,cmap = 'gray')
+plt.show()
 
 #Apply a mask in Y
-masky = np.array([[-1, 0, -1],  #3x3 Sobel Operator
-                  [-2, 0, -2],  #TODO: Upgrade to 5x5, 7x7, etc.
-                  [-1, 0, -1]])
+masky = np.array([[-1, -2, -1], #3x3 Sobel Operator 
+                  [ 0,  0,  0], #TODO: Upgrade to 5x5, 7x7, etc.
+                  [ 1,  2,  1]])
+
+SobelY = np.zeros((ImageLengthX,ImageLengthY), dtype=int)
+
+i=0 
+j=0
+for i in range(ImageLengthX-1):
+    for j in range(ImageLengthY-1):
+        InputMini = np.array([[InputBW.item(i-1, j-1),InputBW.item(i-1, j),InputBW.item(i-1, j+1)],
+                                 [InputBW.item(i,   j-1),InputBW.item(i,   j),InputBW.item(i,   j+1)],
+                                 [InputBW.item(i+1, j-1),InputBW.item(i+1, j),InputBW.item(i+1, j+1)]])
+        #edge (literally) case
+        if i-1 < 0:
+            InputMini[0,:] = 0
+        if j-1 < 0:
+            InputMini[:,0] = 0
+        if i+1 > ImageLengthX:
+            InputMini[2,:] = 0
+        if j+1 >ImageLengthY:
+            InputMini[:,2] = 0
+        
+        SobVal = np.sum(np.multiply(InputMini,masky))
+        if SobVal < 0:
+            SobelY[i,j] = 255
+        elif SobVal >= 0:
+            SobelY[i,j] = 0
+            
+Gy = np.array(SobelY)            
+plt.figure()
+plt.imshow(Gy,cmap = 'gray')
+plt.show()
 
 #Normalize the results of both masks
-
-
-#Plot the results
+Sobel = np.hypot(Gx,Gy)
+plt.figure()
+plt.imshow(Sobel,cmap = 'gray')
+plt.show()
